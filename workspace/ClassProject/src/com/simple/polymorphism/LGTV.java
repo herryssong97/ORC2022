05bb5cd22d95a44769198b79243fcdb75ebc7b9b
herryssong97 <76979784+herryssong97@users.noreply.github.com>
2022-02-24 23:14:15 +0900

    Add files via upload
    2022-02-24

---
+++ workspace/ClassProject/src/com/simple/polymorphism/LGTV.java
@@ -0,0 +1,21 @@
+package com.simple.polymorphism;
+
+public class LGTV implements TV{
+	
+	public void powerOn() {
+		System.out.println("LGTV 전원 켠다..");
+	}
+	
+	public void powerOff() {
+		System.out.println("LGTV 전원 끈다..");
+	}	
+	
+	public void volumeUp() {
+		System.out.println("LGTV 소리 올린다..");
+	}
+	
+	public void volumeDown() {
+		System.out.println("LGTV 소리 내린다..");
+	}	
+
+}
---
+++ workspace/ClassProject/src/com/simple/polymorphism/TV.java
@@ -0,0 +1,9 @@
+package com.simple.polymorphism;
+
+public interface TV {
+	// TV의 규격을 제시
+	public void powerOn();
+	public void powerOff();
+	public void volumeUp();
+	public void volumeDown();
+}
