4c69a86ae10bcf9bee1e0c378151b28da29c62fe
herryssong97 <76979784+herryssong97@users.noreply.github.com>
2022-02-25 09:32:47 +0900

    Update TVUser.java

--- workspace/ClassProject/src/com/simple/polymorphism/TVUser.java
+++ workspace/ClassProject/src/com/simple/polymorphism/TVUser.java
@@ -1,14 +1,14 @@
 package com.simple.polymorphism;
 
 public class TVUser {
-
+	
 	public static void main(String[] args) {
-		// 
+		
 		TV tv = new SamsungTV();
 		
 		tv.powerOn();
 		tv.volumeUp();
 		tv.powerOff();
+		
 	}
-
 }
